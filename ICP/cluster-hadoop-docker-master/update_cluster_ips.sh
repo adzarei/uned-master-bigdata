9a560bec97318cdc3834d3e2adf998caeb4f8c1a
adzarei <adrianzapater@gmail.com>
2019-10-30 22:53:15 +0100

    file renamed to express the action it will do in the future. TODOs added in comments

--- ICP/cluster-hadoop-docker-master/update_cluster_ips.sh
+++ ICP/cluster-hadoop-docker-master/update_cluster_ips.sh
@@ -1,5 +1,17 @@
 #/bin/bash
 
+#Script to update the DNS host file of OSX with the correct ip addresses of each node. WIP
+#TODO: add 'sudo killall -HUP mDNSResponder' at the end to flush the cache.
+#TODO: use sed to update the ip address in /private/etc/hosts of each node automatically.
+
+
+version="1.0"
+header="UPDATE_CLUSTER_IPS.sh"
+
+echo $header
+echo "version:"$version
+echo ""
+echo ""
 echo "Printing IP Address of each node..."
 
 echo "namenode"
