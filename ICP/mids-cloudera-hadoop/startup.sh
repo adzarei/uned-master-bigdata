e0103b58255a01363a4b5f4cda5d7a86369736d5
Adrian <adrianzapater@gmail.com>
2019-11-06 21:51:33 +0100

    docker and startup config updfated

--- ICP/mids-cloudera-hadoop/startup.sh
+++ ICP/mids-cloudera-hadoop/startup.sh
@@ -3,10 +3,13 @@
 echo "###################### Starting Jupyter Notebook ######################"
 source /opt/anaconda/bin/activate
 
-echo "generating config at: /home/$USER/.jupyter/jupyter_notebook_config.py"
-jupyter notebook --generate-config
+#echo "generating config at: ~/.jupyter/jupyter_notebook_config.py"
+#jupyter notebook --generate-config
 
-test -f /home/$USER/.jupyter/jupyter_notebook_config.py
+test -f ~/.jupyter/jupyter_notebook_config.py
 echo "Config folder created: $?"
+
+echo adding password \'uned\' to jupyter.
+sed -i 's/c.NotebookApp.password = ''/c.NotebookApp.password = 'uned'/g' ~/.jupyter/jupyter_notebook_config.py
 
 jupyter notebook --port 8889 --notebook-dir='/media/notebooks' --ip='*' --no-browser &
