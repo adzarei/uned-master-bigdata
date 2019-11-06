9b52534b95a6ee567ce0d6538ae6411ac5324928
adzarei <adrianzapater@gmail.com>
2019-11-06 21:37:30 +0100

    startup.sh file updated to create jupyter configig

--- ICP/mids-cloudera-hadoop/startup.sh
+++ ICP/mids-cloudera-hadoop/startup.sh
@@ -2,4 +2,11 @@
 
 echo "###################### Starting Jupyter Notebook ######################"
 source /opt/anaconda/bin/activate
+
+echo "generating config at: /home/$USER/.jupyter/jupyter_notebook_config.py"
+jupyter notebook --generate-config
+
+test -f /home/$USER/.jupyter/jupyter_notebook_config.py
+echo "Config folder created: $?"
+
 jupyter notebook --port 8889 --notebook-dir='/media/notebooks' --ip='*' --no-browser &
