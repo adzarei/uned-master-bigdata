09ad16120398467bd440c81670ec65b6b6cb6cee
Adrian <adrianzapater@gmail.com>
2020-01-04 23:30:40 +0100

    Exercise 1 a,b y c

--- MED/TP2/aux_functions.R
+++ MED/TP2/aux_functions.R
@@ -1,4 +1,4 @@
-calculate_sano_enf_by_c <- function(c, table_data=read.table("TP2/data/le.txt",header = TRUE), output="both"){
+calculate_sano_enf_by_c <- function(c, table_data, output="both"){
   ind.enf=which(table_data$rta==1)
   ind.sano=which(table_data$rta==0)
   
@@ -8,18 +8,38 @@ calculate_sano_enf_by_c <- function(c, table_data=read.table("TP2/data/le.txt",h
   res.enf=length(which(table_data.enf > c))
   res.sano=length(which(table_data.sano < c))
   
+  # Options: both, sano or enf.
   switch (output,
     both = {
-      print(paste(res.sano, ",",res.enf))
+      paste(res.sano, ",",res.enf)
       
     },
     enf = {
-      print(paste(res.enf))
+      res.enf
       
     },
     sano = {
-      print(paste(res.sano))
+     res.sano
       
     }
   )
 }
+
+
+calculate_ffp <- function(c, r0=33, table_data){
+  calculate_ffp.sano=calculate_sano_enf_by_c(c, table_data, "sano")
+  return((r0 - calculate_ffp.sano) / r0)
+}
+
+
+calculate_fvp <- function(c, r1=20, table_data){
+  calculate_fvp.sano = calculate_sano_enf_by_c(c, table_data, "enf")
+  return(calculate_fvp.sano / r1)
+}
+
+calculate_trapezoidal_auc <- function(){
+  
+  
+}
+
+
--- MED/TP2/tp2_1_a.R
+++ MED/TP2/tp2_1_a.R
@@ -5,6 +5,25 @@ data=read.table("TP2/data/le.txt", header = TRUE)
 
 c_list = read.table("TP2/data/c_num.txt", header=TRUE)
 
+# Columna Sano & Enf
 for (c in c_list$num) {
   print(calculate_sano_enf_by_c(c , data, output = "both"))
 }
+
+# Columna ffp(c)
+for (c in c_list$num) {
+  print(round(calculate_ffp(c = c , table_data = data), digits = 3))
+}
+
+# Columna fvp(c)
+for (c in c_list$num) {
+  print(round(calculate_fvp(c = c , table_data = data), digits = 3))
+}
+
+# Curva ROC AUC:
+
+library(pROC)
+
+roc_obj <- roc(data$rta, data$exp)
+auc(roc_obj)
+plot(roc_obj, print.auc=TRUE)
